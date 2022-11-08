5e3a114ba1639f7a869abb30a59b15bf070fcfbb
farhan sharif <farhansharif1410@gmail.com>
2022-11-09 00:25:14 +0530

    chose your product

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <div>
                     <h1 className="text-5xl font-bold">Order Review Site </h1>
                     <p className="py-6">What is the first thing you do before you go see a movie or buy a product? That is right, chances are, you go and look up some reviews. Reviews have become a big part of our lives, as we reference them for almost everything nowadays.</p>
-                    <button className=" mx-2 px-8 py-2 font-bold border rounded-3xl text-white bg-red-400 hover:bg-white hover:border-red-400 hover:text-red-400 shadow-lg shadow-red-500/50">Get Started</button>
+                    <button className=" mx-2 px-8 py-2 font-bold border rounded-3xl text-white bg-red-400 hover:bg-white hover:border-red-400 hover:text-red-400 shadow-lg shadow-red-500/50">Please Order</button>
                 </div>
             </div>
         </div>
