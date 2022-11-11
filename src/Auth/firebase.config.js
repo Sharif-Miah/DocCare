47ad210342b7e8fb850a1211647756af5d3f7253
farhan sharif <farhansharif1410@gmail.com>
2022-11-11 21:58:05 +0530

    project added

--- src/Auth/firebase.config.js
+++ src/Auth/firebase.config.js
@@ -8,11 +8,7 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
-
-
     storageBucket: process.env.REACT_APP_storageBucket,
-
-
     messagingSenderId: process.env.messagingSenderId,
     appId: process.env.REACT_APP_appId
 };
