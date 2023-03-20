9b9a76fee5807609dc1248a541989c93f0050c30
Sharif-Miah <farhansharif141077@gmail.com>
2023-03-20 20:07:25 +0600

    Contact and update section added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contact from '../Contact/Contact';
 import Features from '../Features/Features';
 import Hero from '../Hero/Hero';
 import MedacalHealth from '../MedacalHealth/MedacalHealth';
@@ -15,6 +16,7 @@ const Home = () => {
             <Profile />
             <Support/>
             <MedacalHealth/>
+            <Contact/>
         </div>
     );
 };
--- src/component/MedacalHealth/MedacalHealth.jsx
+++ src/component/MedacalHealth/MedacalHealth.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const MedacalHealth = () => {
   return (
-    <div className='bg-sky-800 py-52'>
-        <div className='flex justify-center align-center'>
-            <div>
-            <p className='text-white text-3xl'>Showcase Your Medical or Health Oriented <br /> Business with our Beautiful WordPress <br /> Theme</p>
-            <br />
-            <button className='bg-white text-black py-3 px-2 font-bold rounded-3xl hover:bg-sky-500'>Purchase</button>
-            </div>
+    <div className='bg-sky-800 py-10'>
+      
+        <div className="container flex flex-col flex-wrap content-center justify-center p-4 py-20 mx-auto md:p-10">
+          <h1 className="text-5xl antialiased font-semibold leading-none text-center text-gray-100">Get Our Updates</h1>
+          <p className="pt-2 pb-8 text-xl antialiased text-center text-gray-100">Find out about events and other news</p>
+          <div className="flex flex-row">
+            <input type="text" placeholder="example@email.com" className="input input-bordered w-full pr-16 text-black" />
+            <button type="button" className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 bg-violet-700 ml-4 text-white ">Subscribe</button>
+          </div>
         </div>
     </div>
   )
